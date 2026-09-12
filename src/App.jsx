a4721a4bf8f41be85f0ca1cfd3d03952fbf9151c
Jenny Lasquite <lasquitej485@gmail.com>
2026-09-13 01:53:32 +0800

    Task 4: refactor to layout route and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { Routes, Route } from "react-router";
-import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
-import CountriesPage from "./pages/CountriesPage";
-import BucketListPage from "./pages/BucketListPage";
-import AboutPage from "./pages/AboutPage";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="countries" element={<CountriesPage />} />
-        <Route path="bucket-list" element={<BucketListPage />} />
-        <Route path="about" element={<AboutPage />} />
-      </Route>
-    </Routes>
-  );
-};
-
-export default App;
+<Route path="/" element={<Layout />}>
+  <Route index element={<HomePage />} />
+  <Route path="countries" element={<CountriesPage />} />
+  <Route path="bucket-list" element={<BucketListPage />} />
+  <Route path="about" element={<AboutPage />} />
+  <Route path="*" element={<NotFoundPage />} />
+</Route>
