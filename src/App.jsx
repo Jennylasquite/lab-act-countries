98550fa9a1ad78006de96ca22f4161edc580f621
Jenny Lasquite <lasquitej485@gmail.com>
2026-09-13 01:55:28 +0800

    Task 5: convert nav links to Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-<Route path="/" element={<Layout />}>
-  <Route index element={<HomePage />} />
-  <Route path="countries" element={<CountriesPage />} />
-  <Route path="bucket-list" element={<BucketListPage />} />
-  <Route path="about" element={<AboutPage />} />
-  <Route path="*" element={<NotFoundPage />} />
-</Route>
+import { Routes, Route } from "react-router";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import CountriesPage from "./pages/CountriesPage";
+import BucketListPage from "./pages/BucketListPage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="countries" element={<CountriesPage />} />
+        <Route path="bucket-list" element={<BucketListPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
